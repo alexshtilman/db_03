1463941ac0cc9e54dd08fcef7b6744e8a19c9afc
Alex Shtilman <gungam@outlook.com>
2020-12-14 22:49:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Write queries on both SQL and Mongo Db. In the queries 3,4 consider applying mechanism of regular expressions
+
+1. Display count of students having marks in the `[70-80]`
+1. Display two student names having most even marks
+1. Display two student names that are worst in Front-End (Assumed that Front-End is any subject that is not related to Java). Estimation should be done according to the sum of all relevant marks
+1. Display two student names that are best in Back-End (Assumed that Back-End is any subject containing Java). Estimation should be done according to the sum of all relevant marks
+1. Display list of all the marks with counts of their occurrences. List should be sorted in the descending order of the occurrences.
---
+++ sql/script.sql
@@ -0,0 +1,45 @@
+-- Display count of students having marks in the [70-80]
+SELECT COUNT(DISTINCT(stid))
+FROM marks
+WHERE mark BETWEEN 70 AND 80;
+--Display two student names having most even marks
+SELECT s.name
+FROM marks m
+    JOIN students s ON s.stid = m.stid
+GROUP by s.stid
+ORDER BY MAX(m.mark) DESC
+LIMIT 2;
+/*
+ Display two student names that are worst in Front-End 
+ (Assumed that Front-End is any subject that is not related to Java).
+ Estimation should be done according to the sum of all relevant marks
+ */
+SELECT st.name
+    FROM marks m
+    JOIN subjects su on m.suid = su.suid
+    JOIN students st on m.stid = st.stid
+WHERE su.subject NOT LIKE '%Java%'
+GROUP by st.name
+ORDER BY SUM(m.mark) ASC
+LIMIT 2;
+/*
+ Display two student names that are best in Back-End 
+ (Assumed that Back-End is any subject containing Java). 
+ Estimation should be done according to the sum of all relevant marks
+ */
+SELECT st.name
+    FROM marks m
+    JOIN subjects su on m.suid = su.suid
+    JOIN students st on m.stid = st.stid
+WHERE su.subject LIKE '%Java%'
+GROUP by st.name
+ORDER BY SUM(m.mark) DESC
+LIMIT 2;
+/*
+ Display list of all the marks with counts of their occurrences.
+ List should be sorted in the descending order of the occurrences.*/
+SELECT subject,
+    COUNT(suid)
+FROM students_marks_subjects
+GROUP BY subject
+ORDER BY 2 DESC, 1 DESC;
